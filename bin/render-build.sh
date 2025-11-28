3da93130630a7c1ded0f20b324f437428b070446
chatbotcat-dotcom <rosita.ordinolacastillo@gmail.com>
2025-11-27 23:32:54 -0500

    Delete bin directory

--- bin/render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-echo "[Render Build] Actualizando paquetes..."
-apt-get update
-
-echo "[Render Build] Instalando wkhtmltopdf..."
-apt-get install -y wkhtmltopdf
